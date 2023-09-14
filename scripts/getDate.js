f00d81bde56304532f49edb407dd5a88efea0be7
Nikum Sabastine <u0113322020@byui.edu>
2023-09-15 00:02:02 +0100

    Home page building assignment completed

---
+++ scripts/getDate.js
@@ -0,0 +1,5 @@
+
+let oLastModif = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = `Lastly Modified: ${oLastModif}`;
+
+document.getElementById("lastModified").style.color = "#FFFFFF";
