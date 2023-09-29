d15462a7ad42c2e8ff64d2a847640a096f0cd8c5
Nikum Sabastine <u0113322020@byui.edu>
2023-09-29 14:54:32 +0100

    Responsive navigation applied to Update home page

--- scripts/getDate.js
+++ scripts/getDate.js
@@ -3,3 +3,20 @@ let oLastModif = new Date(document.lastModified);
 document.getElementById("lastModified").innerHTML = `Lastly Modified: ${oLastModif}`;
 
 document.getElementById("lastModified").style.color = "#FFFFFF";
+
+
+/* JavaScript codes for responsive hamburger menu */ 
+
+ const mainNav = document.querySelector(".navigation");
+ const hamBtn = document.querySelector("#hamburgerBtn");
+
+ hamBtn.addEventListener("click", () => {
+    hamBtn.classList.toggle("open");
+    mainNav.classList.toggle("open");
+ });
+    
+
+// function toggleMenu() {
+//   const mainNav = document.querySelector(".navigation");
+//   mainNav.classList.toggle("responsive");
+// }
