ce09a5fdf50120f976a93d8137b4b5e4ba56d343
Nikum Sabastine <u0113322020@byui.edu>
2023-10-07 22:56:40 +0100

    Developing the chamber of commerce home page

---
+++ chamber/scripts/getDate.js
@@ -0,0 +1,14 @@
+let oLastModif = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = `Lastly Modified: ${oLastModif}`;
+
+// function toggleMenu(){
+//     const dropDown = document.querySelector(".navigation");
+//     dropDown.classList.toggle("open");
+// }
+const hamBtn = document.querySelector("#menu");
+const mainNav = document.querySelector(".navigation");
+
+hamBtn.addEventListener("click", () => {
+    hamBtn.classList.toggle("open");
+    mainNav.classList.toggle("open");
+})
