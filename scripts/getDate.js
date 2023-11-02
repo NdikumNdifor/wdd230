b3629fe144d4e3c778e111dd52c0e409d6a4c7db
Nikum Sabastine <u0113322020@byui.edu>
2023-11-02 21:59:01 +0100

    Using local storage to track the number of visits

--- scripts/getDate.js
+++ scripts/getDate.js
@@ -14,9 +14,38 @@ document.getElementById("lastModified").style.color = "#FFFFFF";
     hamBtn.classList.toggle("open");
     mainNav.classList.toggle("open");
  });
+
+
+ /* Local storage for the information section */
+
+
+// 1. Initialize display element variable
+   const visitDisplay = document.querySelector(".visits");
+
+// 2. Get the stored VALUE for the numVisits-ls KEY in localStorage 
+// if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
+   let numVisits = Number(window.localStorage.getItem("numVisits - ls")) || 0;
+
+// 3. Determine if this is the first visit or display the number of visits
+   if (numVisits !== 0){
+     visitDisplay.textContent = numVisits;
+   }else{
+     visitDisplay.textContent = `This is your first visit, 😍 welcome!`
+   }
+
+// 4. increment the number of visits by one.
+   numVisits++;
+
+// 5. store the new visit total into localStorage, key=numVisits-ls
+   localStorage.setItem("numVisits - ls", numVisits);
     
 
 // function toggleMenu() {
 //   const mainNav = document.querySelector(".navigation");
 //   mainNav.classList.toggle("responsive");
 // }
+
+
+
+
+
