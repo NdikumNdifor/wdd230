2b22e51782c0cc5d4a72c625c6c3b1bbe9afb9cb
Nikum Sabastine <u0113322020@byui.edu>
2023-11-03 00:48:21 +0100

    Discover page for chamber of commerce website

--- chamber/scripts/getDate.js
+++ chamber/scripts/getDate.js
@@ -23,4 +23,52 @@ changeTextAndBackground.addEventListener("change", () => {
     main.style.color = changeTextAndBackground.checked === true? "#FFF": "#000"  
 });
 
+// Inserting the last modified date at thr footer of the discover page
+let oLastModif1 = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = `Lastly Modified: ${oLastModif1}`;
+
+
+// get the date right now in ms
+
+const theDateToday = new Date();
+const currDateInMs = Date.now();
+let lastVisit = 0;
+let msSinceVisit = 0;
+
+// calculate ms since last visit
+
+if (localStorage.getItem("lastVisitDate") == null) {
+    msSinceVisit = 0;
+}
+else {
+    lastVisit = JSON.parse(localStorage.getItem("lastVisitDate"));
+    msSinceVisit = currDateInMs - lastVisit;
+}
+
+//display one of three possible messages based on how many times they have visited
+
+let displayMessage = document.querySelector(".local-storage")
+
+if (msSinceVisit < 600) {
+	displayMessage.textContent = 'Welcome! Let us know if you have any questions.';
+} 
+else if (msSinceVisit < (24 * 3600000)) {
+    displayMessage.textContent = 'Back so soon! Awesome!';
+}
+else {
+    // calculate days since last visit
+    let numOfDaysSinceVisited = Math.round(msSinceVisit / 84600000);
+    if (numOfDaysSinceVisited < 2) {
+        displayMessage.textContent = `You last visited 1 day ago.`;
+    }
+    else {
+        displayMessage.textContent = `You last visited ${numOfDaysSinceVisited} days ago.`;
+    }
+}
+
+// set the local storage 'last visit' to current date 
+
+localStorage.setItem("lastVisitDate", JSON.stringify(currDateInMs));
+
+
 
