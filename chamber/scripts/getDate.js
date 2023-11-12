4e66078575d60ac3400eb0940275dcd4e6bbfc7c
Nikum Sabastine <u0113322020@byui.edu>
2023-11-12 03:23:41 +0100

    Join page complete for chamber of commerce website

--- chamber/scripts/getDate.js
+++ chamber/scripts/getDate.js
@@ -1,3 +1,6 @@
+
+/*----------------- SCRIPT FOR DATE AND HAMBURGER MENU -------------*/
+
 let oLastModif = new Date(document.lastModified);
 document.getElementById("lastModified").innerHTML = `Lastly Modified: ${oLastModif}`;
 
@@ -14,7 +17,8 @@ hamBtn.addEventListener("click", () => {
 })
 
 
-// script for the toggle switch
+/* ----------------- SCRIPT FOR TOOGLE SWITCH ----------------- */
+
 const changeTextAndBackground = document.querySelector("#checked");
 const main = document.querySelector("main");
     
@@ -22,6 +26,9 @@ changeTextAndBackground.addEventListener("change", () => {
     main.style.backgroundColor = changeTextAndBackground.checked === true? "#000": "#FFF"
     main.style.color = changeTextAndBackground.checked === true? "#FFF": "#000"  
 });
+
+
+/* -------------- SCRIPT FOR DISCOVER PAGE --------------------*/
 
 // Inserting the last modified date at thr footer of the discover page
 let oLastModif1 = new Date(document.lastModified);
@@ -69,6 +76,52 @@ else {
 // set the local storage 'last visit' to current date 
 
 localStorage.setItem("lastVisitDate", JSON.stringify(currDateInMs));
+
+
+
+/* ------------ JAVASCRIPT FOR CHAMBER OF COMMERCE'S DIRECTORY PAGE ------------- */
+
+const baseURL = "https://ndikumndifor.github.io/wdd230/";
+const membersURL = "https://ndikumndifor.github.io/wdd230/chamber/data/members.json";
+const cards = document.querySelector("#cards");
+
+async function getMemebersData() {
+    const response = await fetch(membersURL);
+    const membersData = response.json();
+    displayMembersData(membersData.members);
+}
+displayMembersData();
+
+const displayMembersData =  (members) => {
+    members.forEach( (member) => {
+        
+        const card = document.createElement("section");
+        const logo = document.createElement("img");
+
+        logo.setAttribute("src", member.iconUrl);
+        logo.setAttribute("loading", "lazy");
+        logo.setAttribute("alt", `Logo of ${member.name} company`);
+        logo.setAttribute("width", "140");
+        logo.setAttribute("height", "68");
+
+        card.appendChild(logo),
+        card.appendChild(member.name);
+        card.appendChild(member.address);
+        card.appendChild(member.phone);
+        card.appendChild(member.membershipLevel);
+
+        cards.appendChild(card);
+    })
+}
+
+/*----------------- Adding Date During form Submission ----------------*/
+const date = document.querySelector("#timestamp");
+
+const timestamp = new Date(); 
+const currentDate = Date.now(); // timestamp is the number of milliseconds since 1970:01:00:00
+date.value = currentDate;
+
+
 
 
 
