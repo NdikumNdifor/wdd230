c411a818dd5100b564e94cd1789e5ca422ed6545
Nikum Sabastine <u0113322020@byui.edu>
2023-12-15 20:47:46 +0100

    Delete js file

--- scooter/scripts/banner-script.js
+++
@@ -1,9 +0,0 @@
-const displayDay = document.querySelector(".banner");
-const close = document.querySelector(".banner-close");
-
-function displayBanner() {
-    close.addEventListener("click", function() {
-        this.closest(".banner").style.display = "none";
-    })
-}
-displayBanner();
